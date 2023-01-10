832cdbe810a7205f80fdaddede75dddc515b9a65
MujorB <mujorb@gmail.com>
2023-01-10 23:05:49 +0100

    Coroutine function that utilize an async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Async Comprehensions """
+
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> typing.Generator[float, None, None]:
+    """ Async Generator """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
